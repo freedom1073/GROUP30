6a875316dff8f56dac7652c34de667232f47435f
YH <huangym1073@gmail.com>
2019-11-25 23:25:33 +0000

    Update Models.py

--- census/models.py
+++ census/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+
+class Sightings(model.Model):
+    
+    Latitude = models.FloatField()
+    
+    Longitude = models.FloatField()
+
+    Unique_Squirrel_ID = models.CharField()
+
+    SHIFT =(
+            (‘AM’,’AM’),
+            (‘PM’,’PM’),
+            )
+    
+    Shift = models.CharField(choices=SHIFT)
+
+    Date = models.DateField()
+
+    AGE =(
+            (‘Adult’,’Adult’),
+            (‘Juvenile’,’ Juvenile’),
+            )
+
+    Age = models.CharField(choices=AGE)
+
+    FUR_CHOICES=(
+            (‘Gray’,’Gray’),
+            (‘Cinnamon’,’CInnamon’),
+            (’Black’, ‘Black’),
+            )
+
+    Primary_Fur_Color = models.CharField(choices=FUR_CHOICES)
+    
+    LOCATION = (
+            (‘Ground Plane’,’ Ground Plane’),
+            (‘Above Ground’,’ Above Ground’),
+            )
+
+    Location = models.CharField(choices = LOCATION)
+     
+    Specific_Location = models.CharField()
+
+    Running = BooleanField()
+    Chasing = BooleanField()
+    Climbing = BooleanField()
+    Eating = BooleanField()
+    Foraging = BooleanField()
+    Other_Activities = models.CharField()
+    Kuks = BooleanField()
+    Quaas = BooleanField()
+    Moans = BooleanField()
+    Tail_flags = BooleanField()
+    Tail_twitches = BooleanField()
+    Approaches = BooleanField()
+    Indifferent = BooleanField()
+    Runs_from = BooleanField()
+
