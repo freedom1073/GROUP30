db1425b8ce69a8890040ea6ceb8d6f12c4f32b39
YH <huangym1073@gmail.com>
2019-12-07 20:47:33 +0000

    Update stats and change the directory of map.

--- census/views.py
+++ census/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-
-from django.shortcuts import redirect
 from .models import Sightings
 from .forms import SightingsForm
 import json
+from django.db.models import Avg, Max, Min, Count, Sum
 
 def index(request):
     return HttpResponse("It works!")
@@ -15,8 +14,12 @@ def map(request):
     return render(request,'census/map.html',context)
 
 def stats(request):
-    sightings = Sightings.objects.all() 
-    context = {'content': sightings}
+    number = Sightings.objects.all().count()
+    ave_la = Sightings.objects.aggregate(Avg('Latitude'))
+    ave_lo = Sightings.objects.aggregate(Avg('Longitude'))
+    running_probability = Sightings.objects.filter(Running=True).count()/number
+    chasing_probability = Sightings.objects.filter(Chasing=True).count()/number
+    context = {'num':number, 'ave_la':ave_la, 'ave_lo':ave_lo, 'running_pro':running_probability, 'chasing_pro':chasing_probability}
     return render(request, "census/stats.html", context)
 
 def display(request):
