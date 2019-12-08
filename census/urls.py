749d3fd1677bddc1641e13122e7a6913105cc705
Zitong(Maggie) Peng <zitong_peng@outlook.com>
2019-12-08 20:42:32 +0000

    add correct path in census/urls.py

--- census/urls.py
+++ census/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.display),
     path('stats/', views.stats),
     path('map/', views.map),
-    path('<int:squirrel_id>/edit/',views.edit),
+    path('<int:squirrel_id>/',views.edit),
     path('add/',views.add),
     ]
